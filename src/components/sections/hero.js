58d3355c0976f738cbe5c1ae6b2b2709e208a7f4
Om Yadav <omyadav04352@gmail.com>
2025-06-01 01:40:54 +0530

    Added profile picture.

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -77,7 +77,9 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Om Yadav.</h2>;
-  const three = <h3 className="big-heading">Becoming an efficient engineer each day.</h3>;
+  const three = (
+    <h3 className="big-heading">Becoming an efficient &quot;Engineer&quot; each day.</h3>
+  );
   const four = (
     <>
       <p>
